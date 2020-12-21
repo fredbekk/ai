94e92834bae4a2a4e0899eca5b487ca13ad85fa3
Fred Heggenes <fred@almostfa.me>
2020-12-21 15:45:09 +0100

    Initial commit

---
+++ Ai/Ai/Ai/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Ai.Services;
+using Ai.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ai
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Ai/Ai/Ai/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Ai.ViewModels;
+using Ai.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Ai
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
